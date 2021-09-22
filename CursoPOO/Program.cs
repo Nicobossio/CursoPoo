0f0dde5d8decb650bde861c00bc77f961a483365
Nicobossio42 <nicobossior42@gmail.com>
2021-09-22 09:37:02 -0500

    Añadir mas clases

--- CursoPOO/Program.cs
+++ CursoPOO/Program.cs
@@ -16,6 +16,16 @@ namespace CursoPOO
             Persona2.Age = 23;
 
 
+            Rectangle rectangle1 = new Rectangle();
+            rectangle1.basis = 3;
+            rectangle1.height = 7;
+
+            Rectangle rectangle2 = new Rectangle();
+            rectangle2.basis = 7;
+            rectangle2.height = 10;
+
+
+
             Console.WriteLine($"Persona1 se llama {Persona1.Name} y  tiene {Persona1.Age}");
             Console.WriteLine($"Persona1 se llama {Persona2.Name} y  tiene {Persona2.Age}");
             
---
+++ CursoPOO/Rectangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoPOO
+{
+    public class Rectangle
+    {
+        public double basis;
+        public double height;
+    }
+}
